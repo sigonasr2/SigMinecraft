9e8c7b9a912a0e730eeb4a33c53d8410667dbc92
sigonasr2 <sigonasr2@gmail.com>
2013-11-27 12:03:21 -0700

    Add in more test commands for DiabloDropsHook. Added comments detailing how they work. Added more details on the tiers in DiabloDropsHook.java.

--- BankEconomyMod/src/sig/ItemSets/DiabloDropsHook.java
+++ BankEconomyMod/src/sig/ItemSets/DiabloDropsHook.java
@@ -7,7 +7,18 @@ import org.bukkit.inventory.ItemStack;
 import com.modcrafting.diablodrops.DiabloDrops;
 
 public class DiabloDropsHook {
-	enum Tier {Legendary, Lore, Magical, Rare, Set, Unidentified};
+	/**
+	 * 
+	 * (Note that these are defined in /plugins/Diablodrops/tier.yml. They could be modified if you really wanted to!
+	 * Legendary - (Gold Color) Up to 7 enchantments, up to level 10.
+	 * Lore - (Yellow Color) Up to 7 enchantments, up to level 9.
+	 * Magical - (Blue Color) Up to 3 enchantments, up to level 4.
+	 * Rare - (Red Color) Up to 5 enchantments, up to level 5.
+	 * Set - (Green Color) Up to 7 enchantments, up to level 6.
+	 * Unidentified - (Magic Letters) Up to 10 enchantments, up to level 10.
+	 * 
+	 */
+	public enum Tier {Legendary, Lore, Magical, Rare, Set, Unidentified};
 	
 	public static ItemStack getRandomItem() {
 		return DiabloDrops.getInstance().getDropAPI().getItem();
@@ -28,7 +39,7 @@ public class DiabloDropsHook {
 		//Turns an item into a Diablodrops special item.
 		return DiabloDrops.getInstance().getDropAPI().getItem(i);
 	}
-	public void fillChest(Block b, int size) {
+	public static void fillChest(Block b, int size) {
 		//Fills a specified chest in the world with loot.
 		DiabloDrops.getInstance().getDropAPI().fillChest(b, size);
 	}
